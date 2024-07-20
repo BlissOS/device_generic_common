11be3524b4bd913ffac6a6b2d433be912001a0e0
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-20 12:54:12 +0800

    x86: Add low performance optimizations
    * Some VMs are horribly slow (for example, CPU emulation)
    * Not enabled by default, Pass `androidboot.low_perf=1` parameter to enable it
    * Adjustment for Android-x86:
    - Animation will increase to 50% instead of 0%
    - Don't disable setupwizard or bootanimation
    - All of the changes will be applied for Go builds instead.
    
    Change-Id: I527be45ae138abc14d626eb99dc959133a503fa9

--- device.mk
+++ device.mk
@@ -61,15 +61,6 @@ PRODUCT_PROPERTY_OVERRIDES := \
     external_storage.casefold.enabled=1 \
     external_storage.projid.enabled=1
 
-# LMKd
-ifneq ($(BOARD_IS_GO_BUILD),true)
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.lmk.critical_upgrade=true \
-    ro.lmk.use_minfree_levels=true \
-    ro.lmk.use_psi=true \
-    ro.lmk.use_new_strategy=false
-endif
-
 PRODUCT_COPY_FILES := \
     $(if $(wildcard $(PRODUCT_DIR)init.rc),$(PRODUCT_DIR)init.rc:root/init.rc) \
     $(if $(wildcard $(PRODUCT_DIR)init.sh),$(PRODUCT_DIR),$(LOCAL_PATH)/)init.sh:system/etc/init.sh \
@@ -149,6 +140,12 @@ PRODUCT_COPY_FILES += \
 # Copy Misc Config Files
 PRODUCT_COPY_FILES += \
     $(foreach f,$(wildcard $(LOCAL_PATH)/idc/*.idc $(LOCAL_PATH)/keylayout/*.kl),$(f):$(subst $(LOCAL_PATH),system/usr,$(f)))
+
+# Go init
+ifneq ($(BOARD_IS_GO_BUILD),true)
+PRODUCT_COPY_FILES += \
+    $(if $(wildcard $(PRODUCT_DIR)init.low_performance.rc),$(PRODUCT_DIR)init.low_performance.rc,$(LOCAL_PATH)/init.low_performance.rc):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.low_performance.rc
+endif
 
 # Recovery
 PRODUCT_COPY_FILES += \
