beb25bf32622eae396cfe3a69301e56559897140
Yumi Yukimura <me.cafebabe@gmail.com>
2024-08-03 23:36:17 +0800

    virtio-common: sepolicy: Make virtiofs possible to fallback to genfs
    * In case of the backing host filesystem does not support xattrs
    
    Change-Id: Ide646a050f606cd4cd1e388ceb5a83985a9a3fec

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,7 +182,8 @@ DEVICE_MANIFEST_FILE := device/generic/common/manifest.xml
 #                       vendor/intel/proprietary/houdini/sepolicy \
 #                       vendor/google/proprietary/widevine-prebuilt/sepolicy
 #
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/generic/common/sepolicy/plat_private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(LOCAL_COMMON_TREE)/sepolicy/plat_private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(LOCAL_COMMON_TREE)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += device/generic/common/sepolicy/vendor
 
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
