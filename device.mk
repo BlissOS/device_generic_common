f582bda52414c51056cc3c2059c0e2b56f5bc225
hmtheboy154 <buingoc67@gmail.com>
2024-09-30 15:59:50 -0400

    Allow disabling esdfs/sdcardfs bind mounting
    There are reports that people would like to disable sdcardfs bind
    mounting, let's create an option to do so.
    
    Additionally, here's what we will do also:
    - Set 2 props as seen on target/product/emulated_storage.mk
    - Disable bind mounting by default for live boot.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- device.mk
+++ device.mk
@@ -53,7 +53,9 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     audio.safemedia.bypass=true \
     persist.device_config.mglru_native.lru_gen_config=all \
-    persist.sys.zram_enabled=1
+    persist.sys.zram_enabled=1 \
+    external_storage.casefold.enabled=1 \
+    external_storage.projid.enabled=1
 
 # LMKd
 ifneq ($(BOARD_IS_GO_BUILD),true)
@@ -158,7 +160,7 @@ ifeq ($(BOARD_IS_GO_BUILD),true)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-go
 endif
 
-# Enforce privapp-permissions whitelist
+# Force sdcardfs to use esdfs instead
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.sys.sdcardfs=false \
     persist.sys.sdcardfs=force_off
